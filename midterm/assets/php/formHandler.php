06ae413feef6261742bc2123d8af9bf026f19057
Connor Bernard <connorbernard@berkeley.edu>
2022-10-07 02:33:08 -0700

    added js for form response handling

--- midterm/assets/php/formHandler.php
+++ midterm/assets/php/formHandler.php
@@ -1,16 +1,17 @@
+<!--
+ - Copyright (c) 2022
+ -
+ - @summary Form Handler and Response Page
+ - @author Connor Bernard <connorbernard@berkeley.edu>
+ -
+ -->
 <?php
 $user_name = $_POST["name"];
 $user_email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 $email_from = "connorbernard@berkeley.edu";
-$email_subject = "Gypsy's: $subject"
-$email_body = "
-    User: $user_name\n
-    User email: $user_email\n
-    Message: $message\n
-"
-$headers = "From: $email_from";
-$headers .= "Reply-To: $user_email";
-mail("connorbernard@berkeley.edu", $email_subject, $email_body, $headers);
-?>
+$email_subject = "Gypsy's: $subject";
+$email_body = "User: $user_name\nUser email: $user_email\nMessage: $message\n";
+$sent = @mail("connorbernard@berkeley.edu", $email_subject, $email_body);
+header("Location: ../../contact.html?submitted=$sent");
